51fab585b4cb6a9eacec7f7cbd5c782558aa419d
Masayuki Kozawa <masayuki_kozawa@en-japan.com>
2021-12-21 16:30:23 +0900

    fix role decorator

--- microhr/decorators.py
+++ microhr/decorators.py
@@ -3,31 +3,38 @@ from django.contrib.auth.decorators import user_passes_test
 
 from django.core.exceptions import PermissionDenied
 
-def worker_required(function=None, redirect_field_name=None, login_url='top'):
+def worker_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
     '''
-    Decorator for views that checks that the logged in user is a worker,
-    redirects to the log-in page if necessary.
+    Decorator for views that checks that the logged in user is a worker.    
     '''
+    def check(user):
+        if user.is_active and user.is_worker:
+            return True
+        else:
+            raise PermissionDenied
+
     actual_decorator = user_passes_test(
-        lambda u: u.is_active and u.is_worker,
-        login_url=login_url,
-        redirect_field_name=redirect_field_name
+        check, login_url=login_url, redirect_field_name=redirect_field_name
     )
     if function:
         return actual_decorator(function)
     return actual_decorator
 
 
-def company_required(function=None, redirect_field_name=None, login_url='top'):
+def company_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
     '''
-    Decorator for views that checks that the logged in user is a company,
-    redirects to the log-in page if necessary.
+    Decorator for views that checks that the logged in user is a company
     '''
+    def check(user):
+        if user.is_active and user.is_company:
+            return True
+        else:
+            raise PermissionDenied
+
     actual_decorator = user_passes_test(
-        lambda u: u.is_active and u.is_company,
-        login_url=login_url,
-        redirect_field_name=redirect_field_name
+        check, login_url=login_url, redirect_field_name=redirect_field_name
     )
     if function:
         return actual_decorator(function)
     return actual_decorator
+
