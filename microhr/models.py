73488384780614177ea810610c467aea1c3f6c27
Your Name <ke0136ke@gmail.com>
2022-03-10 13:47:29 +0900

    modify application table

--- microhr/models.py
+++ microhr/models.py
@@ -18,11 +18,12 @@ class Work(models.Model):
     def __str__(self):
         return self.title
 
-class Apply(models.Model):
+class Application(models.Model):
     user= models.ForeignKey(settings.AUTH_USER_MODEL,
                                 verbose_name='応募者',
                                 on_delete=models.CASCADE)
     work= models.ForeignKey(Work,
                                 verbose_name='求人',
-                                on_delete=models.CASCADE)
+                                on_delete=models.SET("現在この求人は募集されていません"))
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
